1879c520c983221bb8bf90558e8848469e402893
Sinan Tan <sinantanwork@gmail.com>
2019-12-06 01:41:48 +0300

    creating scraper class

---
+++ eksi_cli/scraper.py
@@ -0,0 +1,17 @@
+import requests
+
+
+class Scraper():
+
+    def __init__(self):
+        self.base_url="https://eksisozluk.com"
+        self.profile_url = "https://eksisozluk.com/biri/"
+
+    def feed(self,feed,limit):
+        pass
+
+    def user(self,username):
+        pass
+
+    def subject(self,subject_name):
+        pass
