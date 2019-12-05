12808d55ffe704432a31cb3e143b9ebe5c509949
Sinan Tan <sinantanwork@gmail.com>
2019-12-06 01:42:16 +0300

    changing stage area

--- eksi_cli/eksi.py
+++ eksi_cli/eksi.py
@@ -1,6 +1,6 @@
 import sys
 from termcolor import colored
-import scraper
+from scraper import Scraper
 
 
 """ for i in range(1,11):
@@ -8,7 +8,30 @@ import scraper
     print(colored(number, 'green') + "Lorem ipsum dolor sit amet")
  """
 
+helpText="""
+    _______  __  ___      _______. __       ______  __       __     ____    ____  __  
+    |   ____||  |/  /     /       ||  |     /      ||  |     |  |    \   \  /   / /_ | 
+    |  |__   |  '  /     |   (----`|  |    |  ,----'|  |     |  |     \   \/   /   | | 
+    |   __|  |    <       \   \    |  |    |  |     |  |     |  |      \      /    | | 
+    |  |____ |  .  \  .----)   |   |  |    |  `----.|  `----.|  |       \    /     | | 
+    |_______||__|\__\ |_______/    |__|     \______||_______||__|        \__/      |_| 
+                                                                                   
 
+        Kullanım -> komut [ayar]
+                Komutlar:
+                gundem [başlık sayısı(opsiyonel)]
+                ara [başlık veya kullanıcı flagi] [başlık veya kullanıcı adı]
+
+                Ayarlar:
+                    gundem:
+                        başlık sayısı <int>
+                    ara:
+                        -b, -baslik
+                        -k, -kullanici
+
+                Örnek kullanım: Kullanıcı adı eksi olan kullanıcıyı ara.
+                        ara -k eksi
+        """
 
 class Eksicli():
 
@@ -17,46 +40,34 @@ class Eksicli():
         self.profile_url = "https://eksisozluk.com/biri/"
 
     def run(self):
-        #print("deneme" + self.list_subject())
-        print("""
-        _______  __  ___      _______. __       ______  __       __     ____    ____  __  
-        |   ____||  |/  /     /       ||  |     /      ||  |     |  |    \   \  /   / /_ | 
-        |  |__   |  '  /     |   (----`|  |    |  ,----'|  |     |  |     \   \/   /   | | 
-        |   __|  |    <       \   \    |  |    |  |     |  |     |  |      \      /    | | 
-        |  |____ |  .  \  .----)   |   |  |    |  `----.|  `----.|  |       \    /     | | 
-        |_______||__|\__\ |_______/    |__|     \______||_______||__|        \__/      |_| 
-
-
-         
-         Kullanım -> komut [ayar]
-
-         Komutlar:
-         gundem [başlık sayısı(opsiyonel)]
-         ara [başlık veya kullanıcı flagi] [başlık veya kullanıcı adı]
-
-         Ayarlar:
-            gundem:
-                başlık sayısı <int>
-            ara:
-                -b, -baslik
-                -k, -kullanici
-
-        Örnek kullanım: Kullanıcı adı eksi olan kullanıcıyı ara.
-                ara -k eksi
-        """)
-        #command = input(">:")
-        if sys.argv[1]=="gundem":
-            print(self.find_subject(sys.argv[2]))
-        
+        if len(sys.argv)>1:
+            if sys.argv[1]=="gundem":
+                if len(sys.argv)>2:
+                    print(self.list_feed(sys.argv[2]))
+                else:
+                    print(self.list_feed())
+            elif sys.argv[1]=="ara":
+                if sys.argv[2]=="-b" or sys.argv[2]=="-baslik":
+                    print(self.find_subject(sys.argv[3]))
+                elif sys.argv[2]=="-k" or sys.argv[2]=="-kullanici":
+                    print(self.find_user(sys.argv[3]))
+                else:
+                    print("Geçersiz komut.")
+            elif sys.argv[1]=="help" or sys.argv[1]=="yardim":
+                print(helpText)
+            else:
+                print("Geçersiz komut.")
+        else:
+            print("Lütfen parametre giriniz.")
 
     def list_feed(self,feed="popular",limit=50):
         return feed
 
     def find_subject(self,subject_name):
-        pass
+        return subject_name
     
     def find_user(self,username):
-        pass
+        return username
 
     
 
